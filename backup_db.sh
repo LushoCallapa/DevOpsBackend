269eaea82dbc291d215e9ffe5db06ad7dd8d593a
LushoCallapa <luiscallapa2004@gmail.com>
2025-11-23 18:09:17 -0400

    Backend Backups

---
+++ backup_db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DB_NAME="myappdb"
+DB_USER="adminuser"
+DB_HOST="172.31.76.165"
+DB_PORT="5432"
+BACKUP_DIR="/home/ubuntu/backend/db_backups"
+DATE=$(date +"%Y%m%d_%H%M%S")
+BACKUP_FILE="$BACKUP_DIR/${DB_NAME}_backup_$DATE.sql"
+
+mkdir -p $BACKUP_DIR
+
+PGPASSWORD="SuperPassword123" pg_dump -U $DB_USER -h $DB_HOST -p $DB_PORT $DB_NAME > $BACKUP_FILE
+
+gzip $BACKUP_FILE
+
+ls -1tr $BACKUP_DIR/*.gz | head -n -7 | xargs -d '\n' rm -f
+
+echo "Backup realizado: $BACKUP_FILE.gz"
