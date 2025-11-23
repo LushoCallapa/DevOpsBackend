ea28863bd0bc9c1f069868a0384c452213dcecab
LushoCallapa <luiscallapa2004@gmail.com>
2025-11-23 14:39:48 -0400

    Backend

--- dist/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const router = (0, express_1.Router)();
-router.post("/register", userController_1.createUser);
-router.post("/login", userController_1.loginUser);
-router.get("/", userController_1.getUsers);
-exports.default = router;
